087cc76f10f5a11341cecdbc4b81a24637a0231b
mumina-abdo <muminaboruabdo@gmail.com>
2024-03-07 11:43:10 +0300

    add files

---
+++ string.js
@@ -0,0 +1,38 @@
+let str = "extravaganza";
+let substring = str.substring(8,);
+console.log(substring);
+
+let oldstring = "The quick for jumped over the lazy dog";
+let stringToAdd = "eat";
+let indexPosition = 4;
+newString = oldstring.slice(0,indexPosition)+ stringToAdd + oldstring.slice(indexPosition);
+console.log(newString);
+
+
+let x = "wonderful";
+let uppercase=x.toUpperCase();
+console.log(uppercase);
+
+let y = "amazing!";
+let Lowercase=y.toLowerCase();
+console.log(Lowercase);
+
+let f = "UndERneath";
+let lowercase = f.toLowerCase();
+console.log(lowercase);
+
+const String1 = "The pupils are reading in the library";
+console.log(String1[11]+""+String[12]+""+String[13]);
+
+const string1 = "The pupils are reading in the library";
+const string = "The child was sitting on the table before it fall";
+
+const wordToFind1 = "are";
+const wordToFind2 = "sitting";
+
+console.log(isWord1present);
+console.log(isWord2present);
+
+
+
+
